3b99217220ecddb980084a3f339bfa14dede5ced
[durvesh wagh] <[durveshwagh11@gmail.com]>
2024-09-21 21:47:24 +0530

    stage-1

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const { userRouter } = require("./router/user")
 const { courseRouter } = require("./router/course")
 const { adminRouter } = require("./router/admin");
+
 
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/course", courseRouter)
 app.use("/api/v1/admin", adminRouter);
 
 
+async function main() {
+    await mongoose.connect(process.env.MY_DATABASE_STRING)
+        console.log("Connected to the database");
 
-app.listen(3000);
+        app.listen(3000, () => {
+            console.log("Server is listening on port 3000");
+        });
+}
+
+main();
