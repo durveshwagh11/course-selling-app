77ef51506213b8225b7f9f0fe4d674e9b0ce549f
[durvesh wagh] <[durveshwagh11@gmail.com]>
2024-09-21 20:50:55 +0530

    commit routers

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const { userRouter } = require("./router/user")
 const { courseRouter } = require("./router/course")
-
+const { adminRouter } = require("./routes/admin");
 
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/course", courseRouter)
+app.use("/api/v1/admin", adminRouter);
 
 
 
