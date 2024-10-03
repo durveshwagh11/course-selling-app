d87e66df7f71c6531bf45f8a54c4441929d54e11
[durvesh wagh] <[durveshwagh11@gmail.com]>
2024-10-03 12:47:18 +0530

    signup

--- router/user.js
+++ router/user.js
@@ -1,13 +1,38 @@
+const bcrypt = require('bcrypt');
 const { Router } = require("express");
-
+const { userModel } = require("../db")
 const userRouter = Router();
 
-userRouter.post("/signin", (req, res) => {
-    res.send("login")
-})
+userRouter.post("/signup", async (req, res) => {
+    const { email, password, firstname, lastname } = req.body; 
 
-userRouter.post("/signup", (req, res) => {
-    res.send("signup")
+    if (!email || !password || !firstname || !lastname) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await userModel.create({    
+            email: email,
+            password: hashedPassword, 
+            firstname: firstname,
+            lastname: lastname
+        });
+
+        res.status(201).json({
+            message: "SignUp Succeeded"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error during signup",
+            error: error.message
+        });
+    }
+});
+
+userRouter.post("/signin", (req, res) => {
+    res.send("signin")
 })
 
 userRouter.get("/purchases", (req, res) => {
